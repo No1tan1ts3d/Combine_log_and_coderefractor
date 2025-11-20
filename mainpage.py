1aa4212ef846ec2aa6653163b7d7ab7b97e0713a
Manjeel <manjeel.raut33@gmail.com>
2025-11-20 20:57:24 +0545

    removed the files

--- mainpage.py
+++ mainpage.py
@@ -12,7 +12,7 @@ st.write("Developed and Updated by Manjil. Tool update date: 2025-10-06")
 
 
 create_page = st.Page("Log-visualizer-fin1.py", title="Log Parser", icon=":material/add_circle:")
-delete_page = st.Page("code-refractor-c.py", title="Code Refractor", icon=":material/delete:")
+delete_page = st.Page("code-refractor-refined.py", title="Code Refractor Re", icon=":material/delete:")
 
 pg = st.navigation([create_page, delete_page])
 st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
